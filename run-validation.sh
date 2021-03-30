248316d7db3ebc122bd63f6e0d18e57ee6a7f746
Dmytro Dovbii <dmytro.d@qualisystems.com>
2021-03-30 15:42:11 +0300

    fix indentation in validate script

--- run-validation.sh
+++ run-validation.sh
@@ -11,24 +11,28 @@ VAR=$(echo "This is test" | tee /dev/tty)
 
 [ -d "./blueprints" ] || (echo "Wrong repo. No blueprints/ directory" && exit 1);
 
-if [ -n "$INPUT_FILESLIST" ]; then
-
-	for path in $INPUT_FILESLIST; do
+if [ -n "$INPUT_FILESLIST" ];
+then
+	for path in $INPUT_FILESLIST;
+	do
 		# highlevel dir
 		FOLDER=$(dirname $path | cut -d/ -f 1);
 
-		if [ $FOLDER = "blueprints" ]; then
+		if [ $FOLDER = "blueprints" ];
+		then
 			# do nothing, just add to validation list 
 			FILES_TO_VALIDATE+=("${path}")
 			
-		elif [ $FOLDER == "applications" ] || [ $FOLDER == "services" ]; then
-		  # find corresponding blueprint
+		elif [ $FOLDER == "applications" ] || [ $FOLDER == "services" ];
+		then
+		  	# find corresponding blueprint
 			resource=$(dirname $path | cut -d/ -f 2)
 			echo "Find blueprints which depend on ${resource}"
       
-      while read bp;
-      do
-        if [[ ! " ${FILES_TO_VALIDATE[@]} " =~ " ${bp} " ]]; then
+      		while read bp;
+			do
+				if [[ ! " ${FILES_TO_VALIDATE[@]} " =~ " ${bp} " ]];
+				then
 					echo "Adding ${bp} to the list"
 					FILES_TO_VALIDATE+=("${bp}")
 				fi
@@ -37,15 +41,18 @@ if [ -n "$INPUT_FILESLIST" ]; then
 			echo "Skipping ${path}"
 		fi
 	done
-  echo "Final list of files to validate"
-  echo ${FILES_TO_VALIDATE[@]}
+
+  	echo "Final list of files to validate:"
+  	echo ${FILES_TO_VALIDATE[@]}
+
 else
 	FILES_TO_VALIDATE=(blueprints/*.yaml)
 fi
 
 err=0
 
-for ((i = 0; i < ${#FILES_TO_VALIDATE[@]}; i++)); do
+for ((i = 0; i < ${#FILES_TO_VALIDATE[@]}; i++));
+do
 	bpname=`echo ${FILES_TO_VALIDATE[$i]} | sed 's,blueprints/,,' | sed 's/.yaml//'`
 	echo "Validating ${bpname}..."
 	colony --token $INPUT_COLONY_TOKEN --space $INPUT_SPACE bp validate "${bpname}" --branch $BRANCH || ((err++))
@@ -53,6 +60,7 @@ done
 
 echo "Number of failed blueptints: ${err}"
 
-if (( $err > 0 )); then
-	  exit 1;
+if (( $err > 0 ));
+then
+	exit 1;
 fi
